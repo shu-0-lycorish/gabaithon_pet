3e1d7b1ddb7a42f6a2ac58053ded40e2c91537b5
shu-0-lycoris <shu.kwn0257b@gmail.com>
2023-03-02 19:31:49 +0900

    add moneypage

--- lib/gabaithon_pet_web/router.ex
+++ lib/gabaithon_pet_web/router.ex
@@ -18,6 +18,7 @@ defmodule GabaithonPetWeb.Router do
     pipe_through :browser
 
     get "/", PageController, :index
+    get "/money", MoneyController, :index
   end
 
   # Other scopes may use custom stacks.
